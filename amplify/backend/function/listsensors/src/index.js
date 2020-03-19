31814188de6cee195adf1a23e88c8347694b8635
David Moser <davmoser@amazon.com>
2020-03-18 21:00:26 -0700

    add to listsensors function and enable correct permissions

--- amplify/backend/function/listsensors/src/index.js
+++ amplify/backend/function/listsensors/src/index.js
@@ -1,28 +1,34 @@
 const AWS = require('aws-sdk');
 const iotClient = new AWS.Iot();
 
-let iotDataClient;
+var region = process.env.REGION;
+
+AWS.config.update({
+    region: region
+});
 
 exports.handler = async (event) => {
-
-    if (!iotDataClient) {
-        const result = await iotClient.describeEndpoint().promise();
-        iotDataClient = new AWS.IotData({endpoint: result.endpointAddress});
-    }
     
     //query all sensors that have reported a shadow and of type water quality sensor
+    //you must have fleet indexing enabled in IoT Core with REGISTRY_AND_SHADOW indexed
+    
     var params = {
         queryString: 'shadow.reported.name:* AND thingTypeName:WATER_QUALITY_SENSOR'
     };
 
-    iotDataClient.searchIndex(params, function(err, data) {
-        if (err) {
-            console.log(err, err.stack);
-            return {error: err}
-        }
-        else {
-            console.log(data);
-            return {items: date}
-        }
-    });
+    try {
+
+        var result = await iotClient.searchIndex(params).promise();
+
+        var shadow = JSON.parse(result.things[0].shadow);
+
+        return shadow.reported;
+    }
+    catch (err) {
+
+        console.log("error: " + err);
+
+        throw err;
+    }
+
 };
