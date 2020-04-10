eb65b3e8bc639945c5fb867ebc3448f485bf6999
David Moser <davmoser@amazon.com>
2020-04-09 18:00:25 -0700

    update AppSync invocation to use HTTP Post rather than Amplify JS which is not supported in Lambda

--- amplify/backend/function/createsensorvalue/src/index.js
+++ amplify/backend/function/createsensorvalue/src/index.js
@@ -7,31 +7,24 @@ var apiIotjumpstartappsyncGraphQLAPIEndpointOutput = process.env.API_IOTJUMPSTAR
 
 Amplify Params - DO NOT EDIT */
 
+const https = require('https');
 const AWS = require('aws-sdk');
-const Amplify = require('aws-amplify')
-
-const API = Amplify.API;
-const graphqlOperation = Amplify.graphqlOperation;
+const urlParse = require("url").URL;
 
 //environment variables
 const region = process.env.REGION
-const endpoint = process.env.API_IOTJUMPSTARTAPPSYNC_GRAPHQLAPIENDPOINTOUTPUT
-
-AWS.config.update({
-    region: region
-});
-
-Amplify.default.configure({
-  aws_appsync_graphqlEndpoint: endpoint,
-  aws_appsync_region: region,
-  aws_appsync_authenticationType: 'AWS_IAM',
-});
+const appsyncUrl = process.env.API_IOTJUMPSTARTAPPSYNC_GRAPHQLAPIENDPOINTOUTPUT
+const endpoint = new urlParse(appsyncUrl).hostname.toString();
 
 exports.handler = async (event) => {
 
   console.log('event received:' + JSON.stringify(event));
   
+  const req = new AWS.HttpRequest(appsyncUrl, region);
+
   //define the graphql mutation to create the sensor values
+  const mutationName = 'CreateSensorValue';
+
   const mutation = `mutation CreateSensorValue(
       $input: CreateSensorValueInput!
       $condition: ModelSensorValueConditionInput
@@ -51,23 +44,52 @@ exports.handler = async (event) => {
     //set a random sensor status 1-3
     let status = Math.floor(Math.random() * 3) + 1;
     
+    //create the mutuation input from the sensor event data
+    const item = {
+      input: {
+        sensorId: event.sensorId,
+        pH: event.data.pH,
+        temperature: event.data.temperature,
+        salinity: event.data.salinity,
+        disolvedO2: event.data.disolvedO2,
+        status: status,
+        timestamp: event.data.timestamp
+      }
+    };
+
     //execute the mutation
     try {
 
-      const response = await API.graphql(graphqlOperation(
-        mutation, {input: {
-          sensorId: event.sensorId,
-          pH: event.data.pH,
-          temperature: event.data.temperature,
-          salinity: event.data.salinity,
-          disolvedO2: event.data.disolvedO2,
-          status: status,
-          timestamp: event.data.timestamp
-        }})
-      );
+      req.method = "POST";
+      req.headers.host = endpoint;
+      req.headers["Content-Type"] = "application/json";
+      req.body = JSON.stringify({
+          query: mutation,
+          operationName: mutationName,
+          variables: item
+      });
 
-      console.log("Successfull mutation");
-      return response
+      const signer = new AWS.Signers.V4(req, "appsync", true);
+      signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
+
+      const data = await new Promise((resolve, reject) => {
+        const httpRequest = https.request({ ...req, host: endpoint }, (result) => {
+            result.on('data', (data) => {
+                resolve(JSON.parse(data.toString()));
+            });
+      });
+
+        httpRequest.write(req.body);
+        httpRequest.end();
+
+      });
+
+      console.log("Successful mutation");
+
+      return {
+          statusCode: 200,
+          body: data
+      };
 
     }
     catch (err) {
