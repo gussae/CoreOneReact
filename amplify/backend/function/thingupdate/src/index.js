691baa1a14a244fb3ded3c7b70950016e11bbee6
Faraz Haider <fh17488@my.bristol.ac.uk>
2020-04-23 19:08:49 +0500

    updated thingupdate lambda

--- amplify/backend/function/thingupdate/src/index.js
+++ amplify/backend/function/thingupdate/src/index.js
@@ -1,15 +1,50 @@
-/* Amplify Params - DO NOT EDIT
-You can access the following resource attributes as environment variables from your Lambda function
-var environment = process.env.ENV
-var region = process.env.REGION
+const AWS = require('aws-sdk');
+const iot = new AWS.Iot();
 
-Amplify Params - DO NOT EDIT */
+
+var region = process.env.REGION;
+
+AWS.config.update({
+    region: region
+});
 
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+
+    //query for the sensor confirming it has a reported shadow
+    //you must have fleet indexing enabled in IoT Core with REGISTRY_AND_SHADOW indexed
+
+    const sensorId = event.arguments.sensorId || "";
+    const desiredState = event.arguments.desiredState || "true";
+
+    try {
+
+            var params = { endpointType: 'iot:Data-ATS'};
+            var result = await iot.describeEndpoint(params).promise();
+            const host = result.endpointAddress;
+            var iotdata = new AWS.IotData({endpoint: host});
+            
+            var params = {
+                //queryString: 'shadow.reported.name:* AND thingName:' + sensorId
+                payload: `{"enabled":"${desiredState}"}` /* Strings will be Base-64 encoded on your behalf */, /* required */
+                thingName: sensorId /* required */
+            };
+            
+            iotdata.updateThingShadow(params, function(err, data) {
+              if (err) {
+                    console.log(err, err.stack); // an error occurred  
+                    return err.stack;
+                }                
+              else 
+              {    
+                console.log(data);           // successful response
+                return data;
+              }
+            });
+    }
+    catch (err) {
+
+        console.log("error: " + err);
+        throw new Error("Error retrieving sensor: " + sensorId);
+    }
 };
+
