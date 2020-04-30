bf4e26fcb397e84e0f52150127499edb108ad76e
Faraz Haider <fh17488@my.bristol.ac.uk>
2020-04-30 14:39:07 +0500

    Tested update shadow. Still have to write in CoreOneOutgoingData table.

--- amplify/backend/function/thingupdate/src/index.js
+++ amplify/backend/function/thingupdate/src/index.js
@@ -10,75 +10,72 @@ AWS.config.update({
 
 exports.handler = async (event) => {
 
-    //query for the sensor confirming it has a reported shadow
+    //query for the device confirming it has a reported shadow
     //you must have fleet indexing enabled in IoT Core with REGISTRY_AND_SHADOW indexed
+    //const SHADOW_TOPIC = "$aws/things/[deviceId]/shadow/update/delta"
 
-    const sensorId = event.arguments.sensorId || "";
-    const enabledState = event.arguments.enabled || "true";
+    const deviceId = event.arguments.device_id || "";
+    const payload = event.arguments.input.payload || "";
+    const timestamp = event.arguments.input.timestamp || "";    
+    const deviceType = payload.DeviceType;
 
     try {
+        //var topic = SHADOW_TOPIC.replace('[deviceId]', deviceId);
 
-            var params = { endpointType: 'iot:Data-ATS'};
-            var result = await iot.describeEndpoint(params).promise();
-            const host = result.endpointAddress;
-            var iotdata = new AWS.IotData({endpoint: host});
-            console.log(host);
-            var shadowDocument = {
-              state: {
-                  reported: {
-                      //name: "",
-                      enabled: enabledState
-                      /*geo: {
-                          latitude: 0,
-                          longitude: 0
-                      }*/
-                  }
-              }
-          }
-            var payload_value = JSON.stringify(shadowDocument);
-            var params = {
-                payload: payload_value /* Strings will be Base-64 encoded on your behalf */, /* required */
-                thingName: sensorId /* required */
-            };
-            console.log("about to update shadow");
-            iotdata.updateThingShadow(params, function(err, data) {
-              if (err) {
-                   console.log("error");
-                    console.log(err, err.stack); // an error occurred  
-                    return err.stack;
-                }                
-              else 
-              {    
+        var params = { endpointType: 'iot:Data-ATS' };
+        var result = await iot.describeEndpoint(params).promise();
+        const host = result.endpointAddress;
+        var iotdata = new AWS.IotData({ endpoint: host });
+        console.log(host);
+        var shadowDocument = {
+            state: {
+                desired: payload
+            }
+        }
+        var payload_value = JSON.stringify(shadowDocument);
+        var params = {
+            payload: payload_value /* Strings will be Base-64 encoded on your behalf */, /* required */
+            thingName: deviceId /* required */
+        };
+        console.log("about to update shadow");
+        iotdata.updateThingShadow(params, function (err, data) {
+            if (err) {
+                console.log("error");
+                console.log(err, err.stack); // an error occurred  
+                return err.stack;
+            }
+            else {
                 console.log("data");
                 console.log(data);           // successful response
-
+/*
                 var params = {
-                    queryString: 'shadow.reported.name:* AND thingName:' + sensorId
+                    queryString: 'shadow.reported.name:* AND thingName:' + deviceId
                 };
-        
+
                 var result = iot.searchIndex(params).promise();
-        
+
                 if (result.things.length > 0) {
-        
+
                     var element = result.things[0];
-        
+
                     var shadow = JSON.parse(element.shadow);
-            
-                    shadow.reported.sensorId = element.thingName;
-            
+
+                    shadow.reported.deviceId = element.thingName;
+
                     return shadow.reported;
+*/
                 }
                 else {
 
-                    throw new Error("Sensor not found:" + sensorId);
-                }                
-              }
-            });
+                    throw new Error("Device not found:" + deviceId);
+                }
+            }
+        });
     }
     catch (err) {
 
         console.log("error: " + err);
-        throw new Error("Error retrieving sensor: " + sensorId);
+        throw new Error("Error retrieving device: " + deviceId);
     }
 };
 
